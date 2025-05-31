92261641885252088bf6af40f50f6051ffeb30de
nitinshukla <nitin.shukla12004@gmail.com>
2025-05-31 22:44:13 +0530

    added the assets for the app

---
+++ app/admin/login/page.tsx
@@ -0,0 +1,10 @@
+const LoginPage = () => {
+  return (
+    <div className="h-[100vh] w-full flex items-center justify-center bg-cover bg-center bg-no-repeat" style={{
+      backgroundImage: "url('/home/home-bg.png')",}}>
+
+    </div>
+  );
+};
+
+export default LoginPage;

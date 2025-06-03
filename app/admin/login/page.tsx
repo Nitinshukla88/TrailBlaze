7d7b075bbb8da462f6338a6deaa664a38f0e4f80
nitinshukla <nitin.shukla12004@gmail.com>
2025-06-03 19:25:37 +0530

    added prisma and connected with neondb

--- app/admin/login/page.tsx
+++ app/admin/login/page.tsx
@@ -1,29 +1,43 @@
 'use client';
-import { Card, CardHeader } from '@heroui/react';
+import { Button, Card, CardBody, CardFooter, CardHeader, Input } from '@heroui/react';
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
+
+import { Architects_Daughter } from 'next/font/google';
+
+const ArchitectsDaughter = Architects_Daughter({
+  weight: '400',
+  style: 'normal',
+  subsets: ['latin'],
+})
+
 const LoginPage = () => {
+  const handleLogin = async () => {
+    console.log("Login clicked");
+  }
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");   
   return (
-    // <div className="relative flex items-center justify-center">
-    //   <img src="/home/home-bg.png" className="absolute inset-0 -z-10 h-[100vh] w-full flex items-center justify-center bg-cover bg-center bg-no-repeat" alt="Background Image"/>
-    //   <div className="absolute inset-0 z-10 flex items-center justify-center h-[100vh] w-full bg-black/60">
-    //   </div>
-    //   <Card className='shadow-2xl bg-opacity-20 w-[480px]'>
-    //     <CardHeader className='flex flex-col gap-1 capitalize text-3xl items-center justify-center'>
-    //       <div>
-    //         <Image src={"/logo.png"} alt="Logo" width={80} height={80} className="cursor-pointer" />
-    //       </div>
-    //     </CardHeader>
-    //   </Card>
-    // </div>
     <div className='h-[100vh] w-full flex items-center justify-center bg-cover bg-center bg-no-repeat relative' style={{ backgroundImage: 'url(/home/home-bg.png)' }}>
-      <div className='absolute inset-0 bg-black/60'></div>
-      <Card className='shadow-2xl bg-opacity-20 w-[480px]'>
+      <div className='absolute inset-0 bg-black/50 backdrop-blur-xs'></div>
+      <Card className='shadow-2xl bg-gray-400/50 w-[480px]'>
         <CardHeader className='flex flex-col gap-1 capitalize text-3xl items-center'>
           <div className='flex flex-col gap-1 capitalize text-3xl items-center'>
             <Image src={"/logo.png"} alt="Logo" width={80} height={80} className="cursor-pointer" />
+            <span className='text-xl uppercase font-medium italic text-white'>
+              <span className={ArchitectsDaughter.className}>TrailBlaze Admin Login</span>
+            </span>
           </div>
         </CardHeader>
+        <CardBody className='flex flex-col items-center w-full justify-center'>
+          <div className='flex flex-col gap-2 w-full'>
+            <Input placeholder='Email' type='email' value={email} onChange={(e) => setEmail(e.target.value)} color="danger"/>
+            <Input placeholder='Password' type='password' value={password} onChange={(e) => setPassword(e.target.value)} color="danger"/>
+          </div>
+        </CardBody>
+        <CardFooter className='flex flex-col gap-2 items-center justify-center'>
+          <Button className='w-full capitalize' color='danger' variant='shadow' size='lg' onClick={handleLogin}>Login</Button>
+        </CardFooter>
       </Card>
     </div>
   );

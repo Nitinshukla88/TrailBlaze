615565ed3be078f57c619418c55c7b044d1f83bb
nitinshukla <nitin.shukla12004@gmail.com>
2025-06-20 17:53:24 +0530

    moved to tailwind v3 and setup puppeteer for scraping

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,10 +1,39 @@
+import { Browser } from "puppeteer";
+import { startLocationScraping } from "./app/scraping";
+
 export const register = async () => {
     if(process.env.NEXT_RUNTIME === "nodejs") {
         const { Worker } = await import("bullmq");
-        const { connection, jobsQueue } = await import("./app/lib/index");
-
+        const { connection, jobsQueue, prisma } = await import("./app/lib/index");
+        const puppeteer = await import("puppeteer");
+        const BROWSER_WS = "wss://brd-customer-hl_374e0849-zone-trailblaze:oxfa0jfzr5pj@brd.superproxy.io:9222";
         new Worker("jobsQueue", async (job) => {
-            console.log({ job });
+            let browser:undefined | Browser = undefined;
+            try {
+                browser = await puppeteer.connect({ browserWSEndpoint: BROWSER_WS });
+                const page = await browser.newPage();
+                if(job.data.jobType.type === "location") {
+                    console.log("Connected ! Navigating to " + job.data.url);
+                    await page.goto(job.data.url);
+                    console.log("Navigated! Scraping page content...");
+                    const packages = await startLocationScraping(page);
+                    console.log({packages})
+                }
+            } catch (error) {
+                console.log(error);
+                await prisma.jobs.update({
+                    where : {
+                        id : job.data.id
+                    },
+                    data : {
+                        isComplete : true,
+                        status : "failed",
+                    }
+                });
+            } finally {
+                await browser?.close();
+                console.log("Browser closed successfully");
+            }
         }, {
             connection, concurrency:10,removeOnComplete:{count : 1000},removeOnFail:{count : 5000}
         })
